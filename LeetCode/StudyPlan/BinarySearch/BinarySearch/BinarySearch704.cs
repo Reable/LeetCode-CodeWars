51ce14c61fa66f9fedf64033acc67b961991e83b
Ivan Razmyslov <razmyslov.ivan.2004@gmail.com>
2025-12-03 22:01:19 +0300

    feat(leetcode_tasks): migrate to C# and add tests for solution 704. BinarySearch

---
+++ LeetCode/StudyPlan/BinarySearch/BinarySearch/BinarySearch704.cs
@@ -0,0 +1,22 @@
+﻿namespace BinarySearch;
+
+public class BinarySearch704 {
+    public int Search(int[] nums, int target)
+    {
+        int result = -1;
+        
+        int l = 0;
+        int r = nums.Length - 1;
+        while (l <= r) {
+            int mid =  l + (r - l) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                r = mid - 1;
+            } 
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return result;
+    }
+
+}
