8cc9fcb755c0dc6239d8a353ed9a8ff23a551c9f
Ivan Razmyslov <76975247+Reable@users.noreply.github.com>
2026-05-03 19:16:35 +0300

    Create 1768.php

---
+++ LeetCode/php/1768.php
@@ -0,0 +1,31 @@
+<?php
+
+function mergeAlternately($word1, $word2) {
+    $result = [];
+    $len = min(strlen($word1), strlen($word2));
+    $p1 = 0;
+    $p2 = 0;
+
+    while($p1 < $len || $p2 < $len) {
+        if($p1 <= $p2){
+            $result[] = $word1[$p1];
+            $p1++;
+        } else {
+            $result[] = $word2[$p2];
+            $p2++;
+        }
+    }
+
+    while($p1 < strlen($word1)) {
+        $result[] = $word1[$p1];
+        $p1++;
+    }
+    while($p2 < strlen($word2)) {
+        $result[] = $word2[$p2];
+        $p2++;
+    }
+
+    return implode('', $result);
+}
+
+echo mergeAlternately("ab", "pqrs");
