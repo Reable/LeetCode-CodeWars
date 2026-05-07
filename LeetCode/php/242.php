2b9067d9d2986f030fb0c7832e09a9823ef10b74
Ivan Razmyslov <razmyslov.ivan.2004@gmail.com>
2026-05-07 22:05:11 +0300

    Create 242.php

---
+++ LeetCode/php/242.php
@@ -0,0 +1,30 @@
+<?php
+
+function isAnagram242($s, $t)
+{
+    if(strlen($s) !== strlen($t)) return false;
+
+    $map = [];
+
+    for ($i = 0; $i < strlen($s); $i++) {
+        if(isset($map[$s[$i]])){
+            $map[$s[$i]] += 1;
+        } else {
+            $map[$s[$i]] = 1;
+        }
+    }
+
+    for($i = 0; $i < strlen($t); $i++) {
+        if(isset($map[$t[$i]])){
+            $map[$t[$i]] -= 1;
+        } else {
+            return false;
+        }
+    }
+
+    return max($map) == 0;
+}
+
+print_r(isAnagram242("anagram", "nagaram"));
+
+print_r(count_chars("anagram", 1));
